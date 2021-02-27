55358caea32412b34c3094b437d7217e0d738e8e
Andreas Schmid <service@aaschmid.de>
2021-02-27 22:41:26 +0100

    freeplane: init at 1.8.11 for darwin
    Signed-off-by: Andreas Schmid <service@aaschmid.de>

--- pkgs/applications/misc/freeplane/default.nix
+++ pkgs/applications/misc/freeplane/default.nix
@@ -1,9 +1,21 @@
-{ stdenv, lib, fetchzip, makeWrapper, jre }:
+{ stdenv, lib, fetchurl, fetchzip, makeWrapper, jre, undmg }:
 
-stdenv.mkDerivation rec {
+let
   pname = "freeplane";
   version = "1.8.11";
+in stdenv.mkDerivation ({
+  inherit pname;
+  inherit version;
 
+  meta = with lib; {
+    description = "Mind-mapping software";
+    homepage = "https://www.freeplane.org/wiki/index.php/Home";
+    license = licenses.gpl2Plus;
+    maintainers = with maintainers; [ maxhbr aaschmid ];
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+
+} // lib.optionalAttrs stdenv.isLinux {
   src = fetchzip {
     url = "https://sourceforge.net/projects/freeplane/files/freeplane%20stable/freeplane_bin-${version}.zip/download#freeplane_bin-${version}.zip";
     sha256 = "tJyJ7LQoeEFakjOgOU6yUA8dlCuXSCvbUB+gRq4ElMw=";
@@ -22,11 +34,22 @@ stdenv.mkDerivation rec {
     runHook postInstall
   '';
 
-  meta = with lib; {
-    description = "Mind-mapping software";
-    homepage = "https://www.freeplane.org/wiki/index.php/Home";
-    license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ maxhbr ];
-    platforms = platforms.linux;
+} // lib.optionalAttrs stdenv.isDarwin {
+  src = fetchurl {
+    url = "https://altushost-swe.dl.sourceforge.net/project/freeplane/freeplane%20stable/Freeplane-${version}.dmg";
+    name = "${pname}-${version}.dmg";
+    sha256 = "0yikdcgh15wdayiqhz1vzdyqfpggv726gz2xa55hs741x108cmaz";
   };
-}
+
+  depsBuildBuild = [ undmg ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out/Applications/${pname}.app"
+    cp -a . "$out/Applications/${pname}.app"
+    chmod a+x "$out/Applications/${pname}.app/Contents/MacOs/Freeplane";
+
+    runHook postInstall
+  '';
+})
